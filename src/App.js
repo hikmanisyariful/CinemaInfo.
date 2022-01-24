07e53a553dec941a7d35e4d973cd368a80e06609
hikmanisyariful <syarifulfajar017@gmail.com>
2022-01-24 09:47:12 +0700

    Connect react router with react app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from "react";
+import * as React from "react";
+import { Routes, Route } from "react-router-dom";
 import "assets/scss/style.scss";
+
+import Home from "pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to Cinema Info</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h2>HOME PAGE</h2>
+    </div>
+  );
+}
