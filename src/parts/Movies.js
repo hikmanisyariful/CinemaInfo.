0f751eb58475587bb7aa37a38f30284f9f62c443
hikmanisyariful <syarifulfajar017@gmail.com>
2022-02-06 16:53:27 +0700

    Movies card scroll done

--- src/parts/Movies.js
+++ src/parts/Movies.js
@@ -1,5 +1,12 @@
 import React from "react";
-import CardMovie from "./CardMovie";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/scss";
+import "swiper/scss/navigation";
+import "swiper/scss/pagination";
+
+import { Navigation, FreeMode } from "swiper";
+
 import { homePages } from "assets/DummyData/homePages";
 
 export default function Movies() {
@@ -7,11 +14,48 @@ export default function Movies() {
 
   return (
     <div className="container">
-      <div className="row">
+      <h3 style={{ marginBottom: "50px" }}>Trending</h3>
+      <Swiper
+        className="row"
+        slidesPerView={6}
+        spaceBetween={30}
+        slidesPerGroup={6}
+        loopFillGroupWithBlank={true}
+        navigation={true}
+        freeMode={true}
+        grabCursor={true}
+        modules={[Navigation, FreeMode]}
+      >
         {trending.map((movie, index) => {
-          return <CardMovie movie={movie} key={`trending-${movie.id}`} />;
+          return (
+            <SwiperSlide
+              className="col-2"
+              key={`trending-${movie.id}-${index}`}
+            >
+              <div
+                className="card d-flex justify-content-center"
+                style={{ background: "none" }}
+              >
+                <div
+                  className="d-flex flex-column justify-content-center align-items-center"
+                  style={{ width: "100%" }}
+                >
+                  <img
+                    src="https://imdb-api.com/images/original/MV5BZWMyYzFjYTYtNTRjYi00OGExLWE2YzgtOGRmYjAxZTU3NzBiXkEyXkFqcGdeQXVyMzQ0MzA0NTM@._V1_Ratio0.7273_AL_.jpg"
+                    alt={movie.title}
+                    style={{
+                      borderRadius: "15px",
+                      width: "100%",
+                      marginBottom: "10px"
+                    }}
+                  />
+                  <h6 className="text-center">{movie.title}</h6>
+                </div>
+              </div>
+            </SwiperSlide>
+          );
         })}
-      </div>
+      </Swiper>
     </div>
   );
 }
