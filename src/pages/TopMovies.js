136a49b1efc09375b87c078d45e2876b56bca1be
hikmanisyariful <syarifulfajar017@gmail.com>
2022-02-18 18:37:49 +0700

    Handle Spoiler pictures in movie id Page

--- src/pages/TopMovies.js
+++ src/pages/TopMovies.js
@@ -2,8 +2,21 @@ import React from "react";
 
 export default function TopMovies() {
   return (
-    <div className="pages-test">
-      <h2>TOP MOVIES PAGE</h2>
+    <div
+      className="pages-test"
+      style={{ marginTop: 100, background: "white", color: "black" }}
+    >
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-8">col-sm-8</div>
+          <div class="col-sm-4">col-sm-4</div>
+        </div>
+        <div class="row">
+          <div class="col">col-sm</div>
+          <div class="col">col-sm</div>
+          <div class="col">col-sm</div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/parts/Pictures.js
@@ -0,0 +1,41 @@
+import React from "react";
+import ButtonLookPictures from "parts/ButtonLookPictures";
+
+export default function Pictures({ data }) {
+  return (
+    <div
+      className="container-fluid "
+      style={{ marginTop: 70, marginBottom: 50 }}
+    >
+      <div className="posters-wrap position-relative mt-5 me-5 row">
+        <div className="background-posters position-absolute top-0 justify-content-center align-items-center row m-0">
+          <div className="col-auto">
+            <ButtonLookPictures data={data} />
+          </div>
+        </div>
+        <div className="row rounded posters">
+          {data.images.map((image, index) => {
+            if (index < 4) {
+              return (
+                <div
+                  className={`${index === 0 ? "col-12" : "col-4"} px-0`}
+                  key={`poster-picture-${index}`}
+                >
+                  <img
+                    src={image.image}
+                    alt={image.title}
+                    style={{
+                      width: "100%",
+                      height: "100%",
+                      objectFit: "fill"
+                    }}
+                  />
+                </div>
+              );
+            }
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
