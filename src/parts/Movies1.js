a6770089f92ff9349ea70e60eca15aadcdb6b970
hikmanisyariful <syarifulfajar017@gmail.com>
2022-03-13 12:18:32 +0700

    Get viewport/window width in ReactJS

---
+++ src/parts/Movies1.js
@@ -0,0 +1,106 @@
+import React, { useState, useEffect } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/scss";
+import "swiper/scss/navigation";
+import "swiper/scss/pagination";
+
+import { Navigation, FreeMode } from "swiper";
+
+import Star from "elements/Star";
+import Button from "elements/Button";
+
+export default function Movies({ movies, label, isSearch }) {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  const updateDimensions = () => {
+    setWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateDimensions);
+    return () => window.removeEventListener("resize", updateDimensions);
+  }, []);
+
+  useEffect(() => {
+    console.log(width);
+  }, [width]);
+
+  return (
+    <div className="container" style={{ marginBottom: "50px" }}>
+      {!isSearch && (
+        <h3 className="text-light" style={{ marginBottom: "50px" }}>
+          {label}
+        </h3>
+      )}
+
+      <Swiper
+        className="row"
+        slidesPerView={6}
+        spaceBetween={30}
+        slidesPerGroup={6}
+        loopFillGroupWithBlank={true}
+        navigation={true}
+        freeMode={true}
+        grabCursor={true}
+        modules={[Navigation, FreeMode]}
+      >
+        {movies.map((movie, index) => {
+          return (
+            <SwiperSlide
+              className="col-2"
+              key={`trending-${movie.id}-${index}`}
+            >
+              <div
+                className="card d-block justify-content-center card-movie"
+                style={{ background: "none" }}
+              >
+                <div
+                  className="d-flex flex-column justify-content-center align-items-center"
+                  style={{ width: "100%" }}
+                >
+                  <img
+                    src={movie.image}
+                    alt={movie.title}
+                    style={{
+                      borderRadius: "15px",
+                      width: "100%",
+                      marginBottom: "10px"
+                    }}
+                  />
+                  <Button
+                    type="link"
+                    href={`/movie/${movie.id}`}
+                    className="stretched-link d-block"
+                  >
+                    <h6 className="text-center text-light">{movie.title}</h6>
+                  </Button>
+
+                  {movie.rate ? (
+                    <>
+                      <Star
+                        value={Number(movie.rate / 2)}
+                        width={17}
+                        height={17}
+                        spacing={4}
+                      />
+                      <p className="text-dark">{movie.rate}</p>
+                    </>
+                  ) : (
+                    <>
+                      {isSearch ? (
+                        <div></div>
+                      ) : (
+                        <p className="text-dark">No rating</p>
+                      )}
+                    </>
+                  )}
+                </div>
+              </div>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </div>
+  );
+}
