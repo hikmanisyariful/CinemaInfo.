c094da4a329fc33aaba39a7d1ba3aa5099df7c2b
hikmanisyariful <syarifulfajar017@gmail.com>
2022-03-15 17:21:59 +0700

    Collections state of array

--- src/reducers/collections.js
+++ src/reducers/collections.js
@@ -1,38 +1,22 @@
 import { ADD_MOVIE, REMOVE_MOVIE, UPDATE_MOVIE } from "actions/collections";
 
-const initialStateCollection = {
-  liked: [],
-  favorite: [],
-  wantToWatch: [],
-  watched: []
-};
-
-export default function collections(state = initialStateCollection, action) {
+export default function collections(state = [], action) {
   switch (action.type) {
     case ADD_MOVIE:
-      return {
-        ...state,
-        [action.collectionType]: state[action.collectionType].concat([
-          action.movie
-        ])
-      };
+      let newCollections = state.concat([action.movie]);
+      return newCollections;
     case REMOVE_MOVIE:
-      return {
-        ...state,
-        [action.collectionType]: state[action.collectionType].filter(
-          movieId => movieId !== action.movie.id
-        )
-      };
+      let renewCollections = state.filter(
+        movie => movie.id !== action.movie.id
+      );
+      return renewCollections;
     case UPDATE_MOVIE:
-      return {
-        ...state,
-        [action.prevCollectionType]: state[action.prevCollectionType].filter(
-          movieId => movieId !== action.movie.id
-        ),
-        [action.nextCollectionType]: state[action.nextCollectionType].concat([
-          action.movie
-        ])
-      };
+      let prevMovie = state.find(movie => movie.id === action.movie.id);
+      let updateMovie = Object.assign(prevMovie, action.movie);
+      let moviesSelection = state.filter(movie => movie.id !== action.movie.id);
+      let updateCollection = moviesSelection.concat([updateMovie]);
+      return updateCollection;
+
     default:
       return state;
   }
